2c9f55e01733c38a5d1eb627e5e26d12066954c8
TruongGiangPham-1 <truonggi@ualberta.ca>
2024-01-07 04:26:15 -0700

    sidebar readd

--- frontend/lachine-mearning/src/App.js
+++ frontend/lachine-mearning/src/App.js
@@ -5,6 +5,8 @@ import React, {useState, useEffect} from 'react';
 
 // import axios
 import  axios from 'axios';
+import ImageList from '@mui/material/ImageList'; 
+import ImageListItem from '@mui/material/ImageListItem';
 
 function App() {
   const [personImage, setPersonImage] = useState(null);
@@ -16,6 +18,8 @@ function App() {
 
 
   const [resultImage, setResultImage] = useState(null);
+
+  const [imgArray, setImgArray] = useState([]);
 
 
   const backendUrl = "http://localhost:8000";
@@ -45,6 +49,13 @@ function App() {
     setClothingImage(URL.createObjectURL(file));
   };
 
+    const resetHandler = (event) => {
+        setPersonImage(null);
+        setClothingImage(null);
+        setPersonImageOriginal(null);
+        setClothingImageOriginal(null);
+    }
+
     const submitImage = () => {
         console.log('submitting image with url: ' + backendUrl + `/user/${userID}/images/`);
         let formData = new FormData();
@@ -64,6 +75,9 @@ function App() {
             let personImgUrl = response.data.personImage;
 
             setResultImage(backendUrl + clothImgUrl);
+
+
+            setImgArray([...imgArray, {link: backendUrl + clothImgUrl}])
         }).catch((error) => {
             console.log(error);
         });
@@ -84,50 +98,71 @@ function App() {
         <h1 className="title">Picture Me</h1>
         <h1 className="explanation">Next generation e-shopping software</h1>
       </div>
-          <header className="App-header">
-            {(!personImage || !clothingImage) ? <section className="content-column">
-              <div className="input-section">
-                {personImage ? (
-                  <img className="image-container"alt="Upload Photo Here" src={personImage} />
-                ) : (
-                  <div>
-                    <button onClick={personInputHandler}>Upload Photo of Person</button>
-                    <input
-                      type="file"
-                      id="personInput"
-                      accept="image/*"
-                      style={{ display: 'none' }}
-                      onChange={personChangeHandler}
-                    />
+          <div className="outerGrid"> 
+              <div className="sideBar">
+                <h1 className=""> WARDROBE</h1> 
+                <ImageList sx={{ width: 350, height: 800}}  
+                    cols={1} rowHeight={170}> 
+                    {imgArray.map((idx) => ( 
+                        <ImageListItem key={idx.link} > 
+                            <img 
+                                src={`${idx.link}?w= 
+                                164&h=164&fit=crop&auto=format`} 
+                                srcSet={`${idx.link}?w= 
+                                164&h=164&fit=crop&auto=format&dpr=2 2x`} 
+                            /> 
+                        </ImageListItem> 
+                    ))}
+                </ImageList>
+              </div>
+              <header className="App-header">
+                {(!personImage || !clothingImage) ? <section className="content-column">
+                  <div className="input-section">
+                    {personImage ? (
+                      <img className="image-container"alt="Upload Photo Here" src={personImage} />
+                    ) : (
+                      <div>
+                        <button onClick={personInputHandler}>Upload Photo of Person</button>
+                        <input
+                          type="file"
+                          id="personInput"
+                          accept="image/*"
+                          style={{ display: 'none' }}
+                          onChange={personChangeHandler}
+                        />
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
 
-              <div className="input-section">
-                {clothingImage ? (
-                  <img className="image-container" alt="Selected Clothing" src={clothingImage} />
-                ) : (
-                  <div>
-                    <button onClick={clothingInputHandler}>Upload Photo of Clothing</button>
-                    <input
-                      type="file"
-                      id="clothingInput"
-                      accept="image/*"
-                      style={{ display: 'none' }}
-                      onChange={clothingChangeHandler}
-                    />
+                  <div className="input-section">
+                    {clothingImage ? (
+                      <img className="image-container" alt="Selected Clothing" src={clothingImage} />
+                    ) : (
+                      <div>
+                        <button onClick={clothingInputHandler}>Upload Photo of Clothing</button>
+                        <input
+                          type="file"
+                          id="clothingInput"
+                          accept="image/*"
+                          style={{ display: 'none' }}
+                          onChange={clothingChangeHandler}
+                        />
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
 
-            </section> : 
-              !resultImage ? <div className="loading-spinner"/> : 
-              <div className="output-section">
-                <img className="image-container" alt="Resulting Image" src={personImage} />
-              </div>
+                </section> : 
+                  !resultImage ? <div className="loading-spinner"/> : 
+                  <div className="output-section">
+                    <img className="image-container" alt="Resulting Image" src={personImage} />
+                    <h1 className="explanation"> RESULTING IMAGE!</h1> 
+                    <button onClick={resetHandler}>reset</button>
+                  </div>
 
-            }
-            </header>
+
+                }
+                </header>
+            </div>
     </div>
   );
 }
